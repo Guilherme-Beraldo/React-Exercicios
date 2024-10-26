c580de7b90a75a73941527705421fd985cbcd358
Guilherme de Lima Beraldo <gui_beraldo@hotmail.com>
2024-10-26 18:47:28 -0300

    att ex letreiro

--- src/Letreiro.js
+++ src/Letreiro.js
@@ -1,9 +1,33 @@
 
+import React, { useState, useEffect } from 'react';
+
 function Letreiro() {
+
+    const mensagem = "Bem vindo à Fatec!";
+    const [textoExibido, setTextoExibido] = useState('');
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setIndex((prevIndex) => {
+                if (prevIndex < mensagem.length) {
+                    setTextoExibido((prevTexto) => prevTexto + mensagem[prevIndex]);
+                    return prevIndex + 1;
+                } else {
+                    clearInterval(intervalId);
+                    return prevIndex;
+                }
+            });
+        }, 500);
+        return () => clearInterval(intervalId); 
+    }, []);
+
     return (
-          <>
-              <h2>Meu Letreiro</h2>
-          </>
+        <div>
+            <div id="letreiro">
+                <p>{textoExibido}</p>
+            </div>
+        </div>
     );
   }
   
